eb88b4948fc0b428de87ef75bc5578d8bc281e35
Shivakumar <shivaasus531@gmail.com>
2023-12-29 20:16:03 +0530

    lets go to Server

---
+++ index.js
@@ -0,0 +1,12 @@
+function mul(){
+   let num1 = document.getElementById("firstNum").value ;
+   let num2 = document.getElementById("SecNum").value ;
+   document.getElementById("resutl").innerHTML= num1*num2 ;
+}
+function div(){
+    
+    let num1 = document.getElementById("firstNum").value ;
+    let num2 = document.getElementById("SecNum").value ;
+    document.getElementById("resutl").innerHTML = num1 / num2 ;
+
+}
